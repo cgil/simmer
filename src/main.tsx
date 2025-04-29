73a476fcaada0421ad6a69ebc7684be009b19379
Carlos Gil <cg@carlosmgil.com>
2025-04-29 16:21:16 -0700

    track vercel analytics in prod

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App.tsx';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { inject } from '@vercel/analytics';
+import config from './config'; // Import the config object
+
+// Only inject analytics in production
+if (config.environment === 'production') {
+    inject();
+}
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
